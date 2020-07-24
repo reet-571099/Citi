494683cb698655e2d426e34f57ed472ee73291b9
SakshiMahajan98 <mahajansakshi98@gmail.com>
2020-07-25 00:03:39 +0530

    Dashboard

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from '../login/login.component';
 import { HomeComponent} from '../home/home.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { CartComponent } from '../cart/cart.component';
+import { LogoutComponent } from '../logout/logout.component';
 
 
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
   { path: 'login',  component: LoginComponent },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'cart',  component: CartComponent },
+  { path: 'logout',   component:LogoutComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
